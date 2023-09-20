c04b10c9a1783a6354e61bea67d17da8b525a518
HyeonJunSong <music9801@naver.com>
2023-09-20 19:14:45 +0900

    feat: 백 연결

--- src/pages/myPage/likes.js
+++ src/pages/myPage/likes.js
@@ -1,6 +1,61 @@
 import "../../styles/my_page/likes.css";
+import { useState, useEffect } from "react";
+import { baseURL } from "./constatns";
 
 export default function Likes() {
+  const [likesIdList, setLikesIdList] = useState([]);
+  useEffect(() => {
+    //userId로 장바구니 id목록 가져오기
+    fetch(`${baseURL}/like/likeInfos?userId=${"aassdd123"}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setLikesIdList(
+          res.map((item) => ({
+            id: item.id,
+            productId: item.productId,
+          }))
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const [likesList, setLikesList] = useState([]);
+  useEffect(() => {
+    fetch(
+      `${baseURL}/products/productsInfo?values=${likesIdList
+        .map((item) => item.productId)
+        .join(",")}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setLikesList(
+          res.map((item) => ({
+            productId: item.productId,
+            productCategoryId: item.productCategoryId,
+            productName: item.productName,
+            productPrice: item.productPrice,
+            productDesc: item.productDesc,
+          }))
+        );
+      });
+  }, [likesIdList]);
+
+  const renderLikesList = () => {
+    let likesRow = [];
+    for (let i = 0; i < likesList.length; i += 4) {
+      likesRow.push(
+        <div className="likesItemRow" key={i + 1}>
+          {likesList.slice(i, i + 4).map((item) => (
+            <LikesItem item={item} key={item.productId} />
+          ))}
+        </div>
+      );
+    }
+    return likesRow;
+  };
+
   return (
     <div className="likesContainer">
       <div className="titleBox">
@@ -12,39 +67,48 @@ export default function Likes() {
         <div className="title">좋아요</div>
       </div>
       <div className="thickDivider" />
-
-      <div className="likesItemRow">
-        <LikesItem />
-        <LikesItem />
-        <LikesItem />
-        <LikesItem />
-      </div>
-
-      <div className="likesItemRow">
-        <LikesItem />
-        <LikesItem />
-        <LikesItem />
-        <LikesItem />
-      </div>
+      {renderLikesList()}
     </div>
   );
 }
 
-function LikesItem() {
+function LikesItem({ item }) {
+  useEffect(() => {
+    fetch(`${baseURL}/products/downloadProductPhotos/${item.productId}?num=1`, {
+      responseType: "arraybuffer",
+    })
+      .then((res) => res.blob())
+      .then((res) => {
+        var file = new File([res], res.type);
+        onImageDownload(file);
+      });
+    // .then((res) => res.json())
+    // .then((res) => {});
+  }, [item.productId]);
+
+  const [imageSrc, setImageSrc] = useState(null);
+
+  const onImageDownload = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      setImageSrc(reader.result || null); // 파일의 컨텐츠
+    };
+  };
+
   return (
     <div className="likesItemContainer">
       <div className="imageBox">
-        <div className="image">
-          <img src={require("../../imgs/my_page/product.png")} alt="product" />
-        </div>
+        <img src={imageSrc} alt="product" className="image" />
         <img
           className="heart"
           src={require("../../imgs/my_page/heart.png")}
           alt="product"
         />
       </div>
-      <div className="description">클래식 B 주르핏 티셔츠</div>
-      <div className="price">45000원</div>
+      <div className="description">{item.productName}</div>
+      <div className="price">{item.productPrice}</div>
     </div>
   );
 }
