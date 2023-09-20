7889624c2b34d821edbeed635a2cd7123d37535a
Sunja-An <fellin1919@naver.com>
2023-09-20 18:43:45 +0900

    Back up An

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import "./App.css";
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Main from "./pages/Main";
-import Sign_in from "./pages/Sign_in";
-import Sign_up from "./pages/Sign_up";
-import MyPage from "./pages/myPage/myPage";
-import OrderDetails from './components/OrderDetails';
-import DeliveryTest from './pages/DeliveryTest';
+import './App.css';
+
+import { 
+  BrowserRouter as 
+  Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import MainHome from './components/Main/MainHome'
+import ShoppingList from './components/Store/List/ShoppingList';
+import MainStore from './components/Store/MainStore';
+import Goods from './components/Store/IndividualGoods/Goods';
+import NotFound from './components/NotFound/NotFound';
+import Purchase from './components/Buying/Purchase';
+
 function App() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Main</Link> |<Link to="/sign_in">Sign_in</Link> |
-        <Link to="/sign_up">Sign_up</Link> |
-        <Link to="/my_page">MyPage</Link> |
-        <Link to="/OrderDetails">OrderDetails</Link> |
-      </nav>
-
-      {/*Routers*/}
-
+    <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/sign_in" element={<Sign_in />} />
-        <Route path="/sign_up" element={<Sign_up />} />
-        <Route path="/my_page" element={<MyPage />} />
-        <Route path= "/OrderDetails" element={<OrderDetails/>} />
-        <Route path= "/DeliveryTest" element={<DeliveryTest/>} />
+        <Route path="/" element={<MainHome />} />
+        <Route path="/store" element={<MainStore />} />
+        <Route path="/shopping/*" element={<ShoppingList />}>
+          <Route path=":itemid" element={<Goods />} />
+        </Route>
+        <Route path="/login" />
+        <Route path="/register" />
+        <Route path="/shopcart" element={} />
+        <Route path="/purchase/:userid" element={<Purchase />}/>
+        <Route path="/mypage" element={} />
+        <Route path="/modeling" element={} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
