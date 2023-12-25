3a0e079c087d8e1545582fdd7e0f0db01a7f3866
Sunja-An <fellin1919@naver.com>
2023-12-26 08:31:57 +0900

    Modify: change the createbrowserrouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+/* Main app design */
 import './App.css';
 
+/* Related to react router */
 import { 
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom';
+
+/* Basilium App data Provider */
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+/* Components and Pages */
 import Header from './components/Header/Header';
 import Mainpage from './pages/MainPage/Mainpage';
 import BestSellar from './pages/BestSellar/BestSellar';
-import Registration from './pages/Registration/Registration';
+import Errorpage from './pages/Errorpage/Errorpage';
 import ShopingList from './pages/Shopping/ShopingList';
 import Product from './pages/Product/Product';
+import Sign_in from './pages/Login/Sign_in';
+import Sign_up from './pages/Login/Sign_up';
 
 const router = createBrowserRouter([
   {
@@ -19,52 +29,46 @@ const router = createBrowserRouter([
     id: 'root',
     children:[
       {
-        index: true,
         id: "Mainpage",
         element: <Mainpage></Mainpage>,
         // loader 추가 예정
       },
-      {
-        path: "login",
-        id: "Login",
-        element: <Sign_in></Sign_in>,
-      },
-      {
-        path: "signup",
-        id: "signup",
-        element: <Sign_up></Sign_up>,
-      }
     ]
   },
+  {
+    path: "/login",
+    id: "login",
+    element: <Sign_in></Sign_in>,
+  },
+  {
+    path: "/signup",
+    id: "signup",
+    element: <Sign_up></Sign_up>,
+  },
+  {
+    path: "/bestsellar",
+    id: "bestsellar",
+    element: <BestSellar></BestSellar>
+  },
+  {
+    path: "/shop",
+    id: "shop",
+    element: <ShopingList></ShopingList>,
+    children:[
+      {
+        index: true,
+        path: "product/:id",
+        element: <Product></Product>,
+      }
+    ]
+  }
 ]);
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route 
-          path="/"
-          element={<Mainpage />}
-        />
-        <Route 
-          path="/bestsellar"
-          element={<BestSellar />}
-        />
-        <Route 
-          path="/store/ct/:num"
-          element={<ShopingList />}
-        />
-        <Route 
-          path="/store/ct/:num/pd/:value"
-          element={<Product />}
-        />
-        <Route 
-          path="/admin/:num"
-          element={<Registration />}
-        />
-      </Routes>
-    </div>
+    <Provider store={configureStore}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   );
 }
 
